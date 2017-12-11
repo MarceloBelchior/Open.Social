a23e23295e579c02014dba9ed230f352618dceff
Marcelo Belchior Dias <marcelo.belchior@outlook.com>
2017-12-11 19:57:17 -0200

    base

---
+++ Open.Social.Core/Interface/IUserManagerStore.cs
@@ -0,0 +1,16 @@
+﻿using Open.Social.Core.Model.User;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Open.Social.Core.Interface
+{
+  public  interface IUserManagerStore
+    {
+        void SaveOrUpdate(Model.TimeSheet.TimeSheet timesheet);
+        ICollection<Model.TimeSheet.TimeSheet> Consult(Expression<Func<Model.TimeSheet.TimeSheet, bool>> expression);
+        void Remove(int timesheetId);
+        Model.TimeSheet.TimeSheet SelectById(int TimeSheetId);
+    }
+}
