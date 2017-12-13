5d3e2f5c23bf18a6cd3979f5c5c5933f80c002d9
Marcelo Belchior Dias <marcelo.belchior@outlook.com>
2017-12-13 16:21:09 -0200

    gulpfile.js

---
+++ Painel/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+gulp.task('copy-files', function () {
+
+    gulp.src('./node_modules/angular/angular.min.js')
+        .pipe(gulp.dest('./wwwroot/npm/'));
+
+});
